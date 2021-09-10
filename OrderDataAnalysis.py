2435512879367201219b85864b86dc331d566c5d
Jessica Fedor <71146773+jafedor@users.noreply.github.com>
2021-09-10 17:14:27 -0400

    Made comments clearer

--- OrderDataAnalysis.py
+++ OrderDataAnalysis.py
@@ -29,46 +29,51 @@ excel_data_df = pandas.read_excel('jafedor-OrderDataAnalysis-205f624/ItemOrderDa
 
 # Prints the name of the person who spent the most and their total spent
 def find_biggest_spender(df):
-    info_dict = {}
+    person_totals = {}
 
-    # Gather total spent for each person and store it in info_dict
+    # Gather total spent for each person and store it in person_totals dictionary
     for row in range(0, len(df.index)):
         name = str(df.iloc[int(row)]['Name'])
         total_spent = float(df.iloc[int(row)]['Total'])
         # print(total_spent)
 
-        if name not in info_dict:  # if this name hasn't been added to dict yet
-            info_dict[name] = total_spent
+        if name not in person_totals:  # if this name hasn't been added to dict yet
+            person_totals[name] = total_spent
         else:  # if this total should be added to previous total for this person
-            info_dict[name] += total_spent
+            person_totals[name] += total_spent
 
-    # print(info_dict)
-    max_spender = max(info_dict, key=info_dict.get)
-    print(f'{max_spender} spent the most, spending ${info_dict[max_spender]}!')
+    # print(person_totals)
+    max_spender = max(person_totals, key=person_totals.get)
+    print(f'{max_spender} spent the most, spending ${person_totals[max_spender]}!')
 
 
-# What were the most popular items ordered in each region?
+# Prints the most popular item purchased in each region.
 def find_top_items_per_region(df):
-    ans_dict = {'East': {}, 'Central': {}, 'West': {}}
+    units_ordered_totals = {'East': {}, 'Central': {}, 'West': {}}
 
+    # Loop through the dataframe and fill in dictionary containing totals for each unit ordered in each region
     for row in range(0, len(df.index)):
         region = str(df.iloc[int(row)]['Region'])
         item = str(df.iloc[int(row)]['Item'])
         units_in_order = int(df.iloc[int(row)]['Units'])
 
-        if item not in ans_dict[region]:
-            ans_dict[region][item] = units_in_order
+        if item not in units_ordered_totals[region]:
+            units_ordered_totals[region][item] = units_in_order
         else:
-            ans_dict[region][item] += units_in_order
+            units_ordered_totals[region][item] += units_in_order
 
     # print a nicely formatted dictionary displaying items ordered per region
     # printer = pprint.PrettyPrinter(width=30)
-    # printer.pprint(ans_dict)
+    # printer.pprint(units_ordered_totals)
 
-    for region in ans_dict.keys():
-        top_item = max(ans_dict[region], key=ans_dict[region].get)
+    # Calculate the key with the max value for each region 
+    for region in units_ordered_totals.keys():
+        top_item = max(units_ordered_totals[region], key=units_ordered_totals[region].get)
         print(f'The most popular item in the {region} region was a {top_item}!')
 
 
+# Who spent the most money and how much did they spend?
 find_biggest_spender(excel_data_df)
+
+# What were the most popular items ordered in each region?
 find_top_items_per_region(excel_data_df)
